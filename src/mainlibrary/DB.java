b0dae86fd3013b0c6e84cf6145690940c7896ba2
Arun Kollara <arunm201192@gmail.com>
2020-04-27 01:18:36 +0100

    Hardcoded credentials moved to properties files.

--- src/mainlibrary/DB.java
+++ src/mainlibrary/DB.java
@@ -9,33 +9,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  *
  * @author bikash
  */
 public class DB {
-
-    public static String user = "root";
-    public static String password = "123456";
-    public static String connection = "jdbc:mysql://localhost:3306/library";
-
     public static Connection getConnection() {
         Connection con = null;
-        try {
+        try (InputStream input = new FileInputStream("/home/b14ckh34rt/Workspace/projects/spad/src/mainlibrary/config.properties")) {
             Properties props = new Properties();
-            props.put("user", user);
-            props.put("password", password);
+            props.load(input);
             props.put("useUnicode", "true");
             props.put("useServerPrepStmts", "false"); // use client-side prepared statement
             props.put("characterEncoding", "UTF-8"); // ensure charset is utf8 here
 
             Class.forName("com.mysql.jdbc.Driver");
+            String connection = props.getProperty("connection");
             con = DriverManager.getConnection(connection, props);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         } catch (Exception e) {
             System.out.println(e);
         }
         return con;
     }
-
 }
