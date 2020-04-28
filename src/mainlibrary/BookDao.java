cf3681797bd25e46ba335f496ab6860c6becfa9d
Arun Kollara <arunm201192@gmail.com>
2020-04-28 22:40:14 +0100

    Fixed method may fail to close db rsrc in BookDao.java

--- src/mainlibrary/BookDao.java
+++ src/mainlibrary/BookDao.java
@@ -7,17 +7,30 @@ import java.sql.ResultSet;
 public class BookDao {
 public static int save(String callno,String name,String author,String publisher,int quantity){
 	int status=0;
-	try{
-		Connection con=DB.getConnection();
-		PreparedStatement ps=con.prepareStatement("insert into books(callno,name,author,publisher,quantity) values(?,?,?,?,?)");
+
+    Connection Con = null;
+    PreparedStatement ps = null;
+
+	try {
+        Con=DB.getConnection();
+		ps=Con.prepareStatement("insert into books(callno,name,author,publisher,quantity) values(?,?,?,?,?)");
 		ps.setString(1,callno);
 		ps.setString(2,name);
 		ps.setString(3,author);
 		ps.setString(4,publisher);
 		ps.setInt(5,quantity);
 		status=ps.executeUpdate();
-		con.close();
-	}catch(Exception e){System.out.println(e);}
+	} catch (Exception e) {
+	    System.out.println(e);
+	} finally {
+        try {
+            ps.close();
+            Con.close();
+        }
+        catch (Exception e) {
+            /* ignored */
+        }
+    }
 	return status;
 }
 
@@ -29,75 +42,140 @@ public static int save(String callno,String name,String author,String publisher,
     public static boolean PublisherValidate( String Publisher)
 {
     boolean status = false;
-    try(Connection con = DB.getConnection()) {
-        PreparedStatement ps = con.prepareStatement("select * from Publisher where PublisherName = ?"); 
+
+    Connection Con = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    try {
+        Con = DB.getConnection();
+        ps = Con.prepareStatement("select * from Publisher where PublisherName = ?");
         ps.setString(1, Publisher);
-        ResultSet rs=ps.executeQuery();
+        rs=ps.executeQuery();
         status=rs.next();
-        con.close();
-    }catch(Exception e){System.out.println(e);}
+    } catch (Exception e) {
+        System.out.println(e);
+    } finally {
+        try {
+            rs.close();
+            ps.close();
+            Con.close();
+        }
+        catch (Exception e) {
+            /* ignored */
+        }
+    }
     return status;
 }
     
     public static int AddPublisher( String Publisher)
     {
         int status= 0;
-        try(Connection con = DB.getConnection()) {
-		PreparedStatement ps=con.prepareStatement("insert into Publisher(PublisherName) values(?)");
-		ps.setString(1,Publisher);
-		status=ps.executeUpdate();
-                con.close();
-	}catch(Exception e){System.out.println(e);}
-	return status;
-        
+
+        Connection Con = null;
+        PreparedStatement ps = null;
+
+        try {
+            Con = DB.getConnection();
+            ps=Con.prepareStatement("insert into Publisher(PublisherName) values(?)");
+            ps.setString(1,Publisher);
+            status=ps.executeUpdate();
+	    } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                ps.close();
+                Con.close();
+            }
+            catch (Exception e) {
+                /* ignored */
+            }
+        }
+	    return status;
     }
 
 
     public static boolean CheckIfBookExist(String BookName, String Author) {
         boolean status = false;
-        try (Connection con = DB.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("select * from Books where BookName=? and Author=?");
+
+        Connection Con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            Con = DB.getConnection();
+            ps = Con.prepareStatement("select * from Books where BookName=? and Author=?");
             ps.setString(1, BookName);
             ps.setString(2, Author);
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             status = rs.next();
-            con.close();
         } catch (Exception e) {
             System.out.println(e);
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+                Con.close();
+            }
+            catch (Exception e) {
+                /* ignored */
+            }
         }
         return status;
     }
 
 
     public static int SaveBook(String BookN, String AuthorN, String PublisherN, String ShelfN, String RowN, String GenreN) {
-            int status= 0;
-        try(Connection con = DB.getConnection()) {
-		PreparedStatement ps=con.prepareStatement("insert into Books(BookName,Author,Genre,Publisher,Shelf, Row) values(?,?,?,?,?,?)");
-		ps.setString(1,BookN);
-                ps.setString(2, AuthorN);
-                ps.setString(3, GenreN);
-                ps.setString(4, PublisherN);
-                ps.setString(5, ShelfN);
-                ps.setString(6, RowN);
-		status=ps.executeUpdate();
-                con.close();
-	}catch(Exception e){System.out.println(e);}
-	return status;    }
-    
-    public static int Delete(int BookID)
-    {
-          int status= 0;
-        try(Connection con = DB.getConnection()) {
-		PreparedStatement ps=con.prepareStatement("DELETE FROM Books where BookID=?");
-		ps.setInt(1,BookID);
-		status=ps.executeUpdate();
-                con.close();
-	}catch(Exception e){System.out.println(e);}
-	return status;
-        
-    }
-            
-    
- 
+        int status= 0;
 
+        Connection Con = null;
+        PreparedStatement ps = null;
+
+        try {
+            Con = DB.getConnection();
+            ps=Con.prepareStatement("insert into Books(BookName,Author,Genre,Publisher,Shelf, Row) values(?,?,?,?,?,?)");
+            ps.setString(1,BookN);
+            ps.setString(2, AuthorN);
+            ps.setString(3, GenreN);
+            ps.setString(4, PublisherN);
+            ps.setString(5, ShelfN);
+            ps.setString(6, RowN);
+            status=ps.executeUpdate();
+	    } catch(Exception e){
+            System.out.println(e);
+        } finally {
+            try {
+                ps.close();
+                Con.close();
+            }
+            catch (Exception e) {
+                /* ignored */
+            }
+        }
+	return status;
+    }
+    
+    public static int Delete(int BookID) {
+        int status= 0;
+
+        Connection Con = null;
+        PreparedStatement ps = null;
+
+        try {
+            ps=Con.prepareStatement("DELETE FROM Books where BookID=?");
+            ps.setInt(1,BookID);
+            status=ps.executeUpdate();
+	    } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                ps.close();
+                Con.close();
+            }
+            catch (Exception e) {
+                /* ignored */
+            }
+        }
+	return status;
+    }
 }
